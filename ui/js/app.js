6aeb1e863f9ff07df7c7fdddfe6d849a36034164
Aadya Singh <68416322+adysis14@users.noreply.github.com>
2023-05-25 22:17:43 +0530

    Update app.js

--- ui/js/app.js
+++ ui/js/app.js
@@ -1,92 +1,122 @@
 function isNumberKey(evt) {
-  var charCode = evt.which ? evt.which : event.keyCode;
-  if (charCode > 31 && (charCode < 48 || charCode > 57)) return false;
-  return true;
+  var charCode = evt.which ? evt.which : event.keyCode
+  if (charCode > 31 && (charCode < 48 || charCode > 57)) return false
+  return true
 }
 
-$("#verify_otp_model").hide();
-$("#errorbox").hide();
+// window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('sign-in-button', {
+//   'size': 'invisible',
+//   'callback': function(response) {
+//     // reCAPTCHA solved, allow signInWithPhoneNumber.
+//     onSignInSubmit();
+//   }
+// });
+
+// var recaptchaResponse = grecaptcha.getResponse(window.recaptchaWidgetId);
+$("#verify_otp_model").hide()
+$("#errorbox").hide()
+
+// phone auth
+window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier("getotp", {
+  size: "invisible",
+  callback: function (response) {
+    // reCAPTCHA solved, allow signInWithPhoneNumber.
+    //onSignInSubmit();
+  },
+})
+// [END appVerifier]
+
+recaptchaVerifier.render().then(function (widgetId) {
+  window.recaptchaWidgetId = widgetId
+  //  updateSignInButtonUI();
+})
 
 var aadhaar_no_phone_no = {
   "73825370001": "915801xxxx",
   681698110987: "9075339422",
   426472598581: "6307220658",
-};
+}
 
 function onSignInSubmit() {
-  var phoneNumber = "+91" + aadhaar_no_phone_no[$("#aadhaar_no").val()];
-  var appVerifier = window.recaptchaVerifier;
+  window.signingIn = true
+  $("#errorbox").hide()
+  // updateSignInButtonUI();
+  var phoneNumber = "+91" + aadhaar_no_phone_no[$("#aadhaar_no").val()]
+  //console.log(phoneNumber);
+  var d = new Date()
+  d.setTime(d.getTime() + 1 * 24 * 60 * 60 * 1000)
+  var expires = "expires=" + d.toUTCString()
+  document.cookie =
+    "aadhaar" + "=" + $("#aadhaar_no").val() + ";" + expires + ";path=/"
 
+  $("#verifyc").text("Enter verification code send to " + phoneNumber)
+  var appVerifier = window.recaptchaVerifier
   firebase
     .auth()
     .signInWithPhoneNumber(phoneNumber, appVerifier)
     .then(function (confirmationResult) {
-      // SMS sent. Prompt user to type the code from the message, then sign in with the code.
-      window.confirmationResult = confirmationResult;
-      $("#enter_aadhaarno").hide();
-      $("#verify_otp_model").show();
-      console.log("OTP sent");
+      // SMS sent. Prompt user to type the code from the message, then sign the
+      // user in with confirmationResult.confirm(code).
+      window.confirmationResult = confirmationResult
+      window.signingIn = false
+      // updateSignInButtonUI();
+      // $('.verification-code-form').show()
+      // $('#hidepf').hide()
+      $("#enter_aadhaarno").hide()
+      $("#verify_otp_model").show()
+      console.log("otp")
     })
     .catch(function (error) {
       // Error; SMS not sent
-      console.error('Error during signInWithPhoneNumber', error);
-      window.alert("Error: " + error.message);
-      $(".verification-code-form").hide();
-    });
+      // $('.main_loader').hide()
+
+      //console.error('Error during signInWithPhoneNumber', error);
+      window.alert("error\n\n" + error)
+      window.signingIn = false
+      //updateSignInFormUI();
+      //updateSignInButtonUI();
+      $(".verification-code-form").hide()
+    })
 }
+// Phone auth end //
 
-// Phone auth initialization
-firebase.initializeApp({
-  apiKey: "AIzaSyCWE6sGJU6CObYhcytNLmtUjYqtsQ2Hg9U",
-  authDomain: "elect-9d431.firebaseapp.com",
-  projectId: "elect-9d431",
-});
-
-var recaptchaVerifier = new firebase.auth.RecaptchaVerifier("getotp", {
-  size: "invisible",
-});
-
-recaptchaVerifier
-  .render()
-  .then(function (widgetId) {
-    window.recaptchaWidgetId = widgetId;
-  })
-  .catch(function (error) {
-    console.error("Error rendering reCAPTCHA widget", error);
-  });
-
-$(document).on("click", "#verifyotp", function () {
-  var code = $("#verify_otp").val();
-  var confirmationResult = window.confirmationResult;
-
+$(verifyotp).click(function () {
+  var code = $("#verify_otp").val()
   confirmationResult
     .confirm(code)
     .then(function (result) {
       // User signed in successfully.
-      var user = result.user;
-      console.log("User UID:", user.uid);
-      window.location = "/info";
+      var user = result.user
+      window.verifyingCode = false
+      //login success
+      console.log(user.uid)
+      var d = new Date()
+      d.setTime(d.getTime() + 1 * 24 * 60 * 60 * 1000)
+      var expires = "expires=" + d.toUTCString()
+      document.cookie = "show" + "=" + user.uid + ";" + expires + ";path=/"
+      window.location = "/info"
     })
     .catch(function (error) {
       // User couldn't sign in (bad verification code?)
-      console.error("Error while checking the verification code", error);
+      console.error("Error while checking the verification code", error)
       window.alert(
         "Error while checking the verification code:\n\n" +
           error.code +
           "\n\n" +
           error.message
-      );
-      $("#errorbox").show();
-      $("#error").text("Enter a valid OTP");
-    });
-});
+      )
+      window.verifyingCode = false
+      $("#errorbox").show()
+      $("#error").text("Enter valid OTP")
+    })
+})
 
-$(document).on("click", "#getotp", function () {
+$(getotp).click(function () {
   if ($("#aadhaar_no").val() == "") {
-    $("#errorbox").show();
-    $("#error").text("Please enter Aadhaar No");
+    $("#errorbox").show()
+    $("#error").text("Please Enter Aadhaar No")
   } else {
-    onSignInSubmit();
-    $("#errorbox").hide();
+    onSignInSubmit()
+    $("#errorbox").hide()
   }
-});
+})
